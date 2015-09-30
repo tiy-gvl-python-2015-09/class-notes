1de03c2a72fe33ece6284814ca0a38e1edec927f
taddeimania <jtaddei@gmail.com>
2015-09-30 12:06:51 -0400

    Added roomba sim notebook

---
+++ Notebooks/week-3/roomba/roomba_sim.py
@@ -0,0 +1,35 @@
+class Room:
+    """
+    Responsibilities:
+    - width
+    - height
+    - keeps track of what squares are clean
+    """
+    pass
+
+
+class Roomba:
+    """
+    Responsibilities:
+    - location
+    - know its current direction
+    - respond to hitting a wall
+    - give its relative x and y movement  ehhhhhh
+    - speed of 1
+    """
+    pass
+
+
+class Simulation:
+    """
+    Responsibilities:
+    - placing the Roomba
+    - asking Roomba where it's moving
+    - updating the room
+    - iterating over turns
+    - reporting data (current turn, percent complete)
+    Collaborators:
+    - Room
+    - Roomba
+    """
+    pass
